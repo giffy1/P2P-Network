ec4563d0132471bed7b601a59342566a7938dea9
giffy1 <giffy1991@gmail.com>
2017-03-07 15:58:45 -0500

    Implemented P2P node class, which manages client and server sockets on separate threads for communicating between peers.

--- client.py
+++ client.py
@@ -16,9 +16,62 @@ shelter.
 
 """
 
+import threading
+import time
 import socket
 
-# create an INET, STREAMing socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# now connect to the server on port 9999
-s.connect(("localhost", 9999))
+class ServerThread(threading.Thread):
+    def __init__(self, address):
+        threading.Thread.__init__(self)
+        self.address = address
+    
+    def run(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind(self.address)
+        self.server_socket.listen(5)
+            
+        print 'waiting for connection...'
+        cs, addr = self.server_socket.accept()
+        print '...connected from:', addr
+        while True:
+            message = cs.recv(1024)
+            if message:
+                print "received: " + str(message)
+
+class ClientThread(threading.Thread):
+    def __init__(self, address):
+        threading.Thread.__init__(self)
+        self.address = address
+    
+    def run(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(self.address)
+        time.sleep(2)
+        self.client_socket.send("hello")
+
+class P2PNode():
+    """
+    Represents a node in a peer-to-peer network. The network is a circular, 
+    singly-connected network, that is each node is connected to exactly two 
+    other nodes in the network. Thus each node acts as both a client and 
+    server.
+    
+    """
+    def __init__(self, address):
+        self.address = address
+    
+    def connect(self, peer):
+        peer.listen()
+        client_thread = ClientThread(peer.address)
+        client_thread.start()
+        
+    def listen(self):
+        print "starting server thread with address " + str(self.address)
+        server_thread = ServerThread(self.address)
+        server_thread.start()
+        
+node1 = P2PNode(('localhost', 9999))
+node2 = P2PNode(('localhost', 8888))
+
+node1.connect(node2)
