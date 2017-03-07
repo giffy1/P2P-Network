54353e2ae3ecb8ee50cd52cdbc3d8b695e14992f
giffy1 <giffy1991@gmail.com>
2017-03-06 19:21:46 -0500

    Updated initial documentation.

--- client.py
+++ client.py
@@ -2,12 +2,23 @@
 """
 Created on Mon Mar  6 18:55:32 2017
 
-@author: snoran
+Sean Noran
+Distributed Operating Systems
+Lab 1 P2P Network
+
+This is an implementation of a P2P Network, where nodes are represented 
+by pigs in a grid. Angry birds are tossed at the grid and pigs must 
+communicate the predicted landing coordinates of the birds in order to 
+save their fellow pigs. This is done in a peer-to-peer network, where 
+pigs can broadcast a bird's landing location (within a certain number of 
+peer-to-peer hops) and each pig can inform physical neighbors to take 
+shelter.
+
 """
 
 import socket
 
 # create an INET, STREAMing socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# now connect to the web server on port 80 - the normal http port
+# now connect to the server on port 9999
 s.connect(("localhost", 9999))
