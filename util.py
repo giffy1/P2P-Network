6a0f951ef62c0f448fb6f8e102dd520fe18133b8
giffy1 <giffy1991@gmail.com>
2017-03-10 22:19:44 -0500

    Added generate_random_coordinate() method.

--- util.py
+++ util.py
@@ -12,3 +12,9 @@ def manhattan_distance(location1, location2):
     Returns the Manhattan distance between two locations.
     """
     return np.sum(np.abs(np.subtract(location1, location2)))
+    
+def generate_random_coordinate(grid_size):
+    """
+    Generates a random coordinate in a 2D grid with the given dimensions.
+    """
+    return (np.random.randint(0,grid_size[0]), np.random.randint(0,grid_size[1]))
