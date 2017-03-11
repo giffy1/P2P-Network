1331a32eeb7685b6973f89c463ce32ead983081b
giffy1 <giffy1991@gmail.com>
2017-03-10 22:17:31 -0500

    Migrated generate_random_coordinate() function to util file, but it really isn't in use any more because I selected specific grid configurations.

--- main.py
+++ main.py
@@ -20,21 +20,14 @@ import time
 import numpy as np
 from pig import Pig
 import p2p
-from util import manhattan_distance
+from util import generate_random_coordinate
+from params import grid_size, pillar_locations, damage_by_landing, damage_other, pig_locations, bird_landing, bird_time_of_flight, n_pigs
 
 p2p.exit_flag = 0 # set to 1 to indicate that P2P communication threads should terminate
 
-grid_size = (4,4) # we'll work with a 5 x 5 grid to start
 grid = np.zeros(grid_size)
-pillar_locations = [(1,0), (3,3)]
 for pillar_loc in pillar_locations:
     grid[pillar_loc] = -1
-
-def generate_random_coordinate(grid_size):
-    """
-    Generates a random coordinate in a 2D grid with the given dimensions.
-    """
-    return (np.random.randint(0,grid_size[0]), np.random.randint(0,grid_size[1]))
     
 def get_pig_by_location(pigs, location):
     for pig in pigs:
@@ -61,17 +54,8 @@ def get_open_neighbors(location):
             open_neighbors.append(neighbor)
     return open_neighbors
 
-# indicates the amount of damage a pig suffers if hit by the bird's landing:
-damage_by_landing = 2
-# indicates the amount of damage a pig suffers if adjacent to a pig affected by the landing:
-damage_by_adjacent_pig = 1
 
-bird_landing = (2,3) # generate_random_coordinate(grid_size)
-bird_time_of_flight = 3 # number of seconds the bird requires to land
-
-n_pigs = 6
 pigs = []
-pig_locations=[(0,0), (0,3), (1,3), (2,2), (2,3), (3,2)]
 # TODO : if the first pig is the one hit, then currently, it won't respond properly
 location = (-1,-1) # initial coordinates outside of the grid to ensure we enter the generation loop
 for i in range(n_pigs):
@@ -89,7 +73,6 @@ for pig in pigs[1:]:
     prev_pig.connect(pig)
     prev_pig = pig
 prev_pig.connect(pigs[0])
-time.sleep(1)
 
 print "Bird landing at location {}".format(bird_landing)
 print "Starting locations:"
@@ -99,6 +82,8 @@ pigs[0].broadcast_bird_approaching(bird_landing, 3)
 
 time.sleep(bird_time_of_flight)
 
+if (get_pig_by_location(pigs, bird_landing)):
+    get_pig_by_location(pigs, bird_landing).set_open_neighbors([])
 # refresh grid and decrement status of each pig if hit
 grid = np.zeros(grid_size)
 for pillar_loc in pillar_locations:
@@ -115,7 +100,7 @@ def propagate_hit(location):
         if hit_grid[neighbor] == 1:
             continue
         elif grid[neighbor] > 0:
-            get_pig_by_location(pigs, neighbor).status -= damage_by_adjacent_pig
+            get_pig_by_location(pigs, neighbor).status -= damage_other
             hit_grid[neighbor]=1
         elif grid[neighbor] < 0:
             hit_grid[neighbor]=1
@@ -127,18 +112,11 @@ if grid[bird_landing] > 0:
     propagate_hit(bird_landing)
     
 pigs[2].request_status(4)
-#for pig in pigs:
-#    print pig.get_id(), pig.status
-
-time.sleep(3)
-
 pigs[0].request_status_all()
 
+# wait until requests finish
 time.sleep(5)
-p2p.exit_flag = 1
     
 print grid
-#node1 = P2PNode(('localhost', 9999))
-#node2 = P2PNode(('localhost', 8888))
-#
-#node1.connect(node2)
+
+p2p.exit_flag = 1
